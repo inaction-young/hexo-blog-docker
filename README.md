b4a985ad53a86726072ab77c74699b22d30976dc
tj408122155@163.com <tj408122155@163.com>
2025-06-25 11:54:49 +0800

    简化

--- README.md
+++ README.md
@@ -8,12 +8,15 @@ hexo docker file
 * 默认端口：4000
 
 #### 说明
-* 镜像初次启动会在工作目录下初始化hexo，会执行`hexo init blog`，不会安装任何模版
+* 容器初次启动会在工作目录下初始化hexo，会执行`hexo init blog`，不会安装任何模版
 * 所以需要自己将`/app/blog`映射出来，将模版、内容放在对应的目录下
 * 后续启动发现有/app/blog目录则不会再初始化hexo
 
-#### Hexo
-* https://hexo.io/zh-cn/
+#### 参考
+* Hexo官网：https://hexo.io/zh-cn/
+* 镜像的脚步参考了：https://github.com/appotry/docker-hexo
+
+
 
 
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#echo "***** Starting server on port ${HEXO_SERVER_PORT} *****" 
-#hexo server -d -p ${HEXO_SERVER_PORT}
-
-#pm2 start /hexo_run.js
-
-#echo "***** stop hexo server run:  pm2 stop /hexo_run.js  *****" 
-#echo "***** start hexo server run:  pm2 start /hexo_run.js  *****" 
-
-
 cnpm config set registry https://registry.npmmirror.com/
 
 mkdir -p /app/blog
@@ -31,39 +22,12 @@ else
 #        hexo-generator-index \
 #        gulp-clean-css gulp-uglify gulp-htmlmin gulp-htmlclean 
         
-    #cnpm install hexo-generator-index-pin-top --save 
-    #cnpm install hexo-offline --save 
-    #cnpm install hexo-web-push-notification --save 
-    #cnpm install highlight.js --save 
-    #echo "install live2d and model weier" 
-    #cnpm uninstall fsevents 
-    #cnpm install fsevents --save 
-    #echo "Please install browser plugin liveReload !" 
-    #cnpm install livereload bufferutil utf-8-validate --save 
-    #cnpm install hexo-helper-live2d --save 
-    #cnpm install live2d-widget-model-lwet --save 
-    #cnpm install livereload bufferutil utf-8-validate --save
-    #echo "install hexo-theme-matery" 
-    #git clone https://github.com/blinkfox/hexo-theme-matery.git /app/themes/matery; 
 fi; 
 
 cd /app/blog
 hexo server
 
-#if [ ! -f /app/requirements.txt ]; then 
-#    echo "***** App directory contains no requirements.txt file, continuing *****"; 
-#else 
-#    echo "***** App directory contains a requirements.txt file, installing cnpm requirements *****"; 
-#    cat /app/requirements.txt | xargs cnpm --prefer-offline install --save; 
-#fi; 
 
-#if [ ! -f /app/userRun.sh ]; then 
-#    echo "cp userRun.sh"
-#    cp /userRun.sh /app/userRun.sh; 
-#    chmod +x /app/userRun.sh;
-#    /app/userRun.sh; 
-#else 
-#    echo "run userRun.sh"
-#    /app/userRun.sh; 
-#fi
-#pm2 logs hexo_run
+
+
+
